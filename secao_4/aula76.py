61f3a43b2c3f8c96825722129e71e758b0cf19b0
Marcos Albano <marcos.telematica@gmail.com>
2023-06-26 11:59:50 -0300

    Alteracao na aula76.py

--- secao_4/aula76.py
+++ secao_4/aula76.py
@@ -21,21 +21,73 @@
 #     ]
 # }
 # pessoa = dict(nome='Luiz Otávio', sobrenome='Miranda')
-pessoa = {
-    'nome': 'Luiz Otávio',
-    'sobrenome': 'Miranda',
-    'idade': 18,
-    'altura': 1.8,
-    'endereços': [
-        {'rua': 'tal tal', 'número': 123},
-        {'rua': 'outra rua', 'número': 321},
-    ],
-}
+# pessoa = {
+#     'nome': 'Luiz Otávio',
+#     'sobrenome': 'Miranda',
+#     'idade': 18,
+#     'altura': 1.8,
+#     'endereços': [
+#         {'rua': 'tal tal', 'número': 123},
+#         {'rua': 'outra rua', 'número': 321},
+#     ],
+# }
 # print(pessoa, type(pessoa))
-print(pessoa['nome'])
-print(pessoa['sobrenome'])
+# print(pessoa['nome'])
+# print(pessoa['sobrenome'])
+# print()
+# for chave in pessoa:
+#     print(chave, ' - ', pessoa[chave])
+# pessoa = {}
+# chave = 'nome'
+# pessoa[chave] = 'Albano'
+# pessoa['sobrenome'] = 'Maia'
 
-print()
+# pessoa[chave] = 'Maria'
+# del pessoa['sobrenome']
 
-for chave in pessoa:
-    print(chave, pessoa[chave])
+# print(pessoa['nome'])
+# if pessoa.get('sobrenome') is None:
+#     print('NÃO EXISTE')
+# else:
+#     print(pessoa['sobrenome'])
+###################################################################
+
+# Métodos úteis dos dicionários em Python
+# len - quantas chaves
+# keys - iterável com as chaves
+# values - iterável com os valores
+# items - iterável com chaves e valores
+# setdefault - adiciona valor se a chave não existe
+# copy - retorna uma cópia rasa (shallow copy)
+# get - obtém uma chave
+# pop - Apaga um item com a chave especificada (del)
+# popitem - Apaga o último item adicionado
+# update - Atualiza um dicionário com outro
+
+# print(len(pessoa))
+# print(tuple(pessoa.keys()))
+# print(list(pessoa.keys()))
+# print(list(pessoa.values()))
+# print(list(pessoa.items()))
+# for valor in pessoa.values():
+#     print(valor)
+# for chave, valor in pessoa.items():
+#     print(chave, valor)
+import copy
+
+d1 = {
+    'c1': 1,
+    'c2': 2,
+    'l1': [0, 1, 2]
+}
+d2 = d1.copy()
+# d2 = copy.deepcopy(d1)
+
+d2['l1'][1] = 999999
+
+d2 ['c1'] = 100
+print(d1)
+print(d2)
+
+
+
