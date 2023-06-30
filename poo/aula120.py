85303aced0a03b66cc4a8d03e59ac1bf38f74974
Marcos Albano <marcos.telematica@gmail.com>
2023-06-30 12:59:02 -0300

    Criada pasta poo

---
+++ poo/aula120.py
@@ -0,0 +1,10 @@
+﻿# class - Classes são moldes para criar novos objetos
+# As classes geram novos objetos (instâncias) que
+# podem ter seus próprios atributos e métodos.
+# Os objetos gerados pela classe podem usar seus dados
+# internos para realizar várias ações.
+# Por convenção, usamos PascalCase para nomes de
+# classes.
+# string = 'Luiz'  # str
+# print(string.upper())
+# print(isinstance(string, str))
