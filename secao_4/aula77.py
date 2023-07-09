0804b511bca75207f7a08effe4a2cea9a652d66a
Marcos Albano <marcos.telematica@gmail.com>
2023-07-09 15:51:05 -0300

    Add aula 77

--- secao_4/aula77.py
+++ secao_4/aula77.py
@@ -19,12 +19,38 @@ perguntas = [
     },
 ]
 
+qtd_acertos = 0
 for pergunta in perguntas:
-    print('Pergunta', pergunta['Pergunta'])
+    print('Pergunta:', pergunta['Pergunta'])
     print()
-    for opcao in pergunta['Opções']:
-        ...
+    
+    opcoes = pergunta['Opções']
+    for i, opcao in enumerate(opcoes):
+        print(f'{i})', opcao)
     print()
-
-
+    
+    escolha = input('Escolha uma opção: ')
+    
+    acertou = False
+    escolha_int = None
+    qtd_opcoes = len(opcoes)
+    
+    if escolha.isdigit():
+        escolha_int = int(escolha)
+        
+    if escolha_int is not None:
+        if escolha_int >= 0 and escolha_int < qtd_opcoes:
+            if opcoes[escolha_int] == pergunta['Resposta']:
+                acertou = True
+                
+    if acertou:
+        qtd_acertos += 1
+        print('Acertou')
+    else:
+        print('Errou')
+    
+    print()
+    
+print('Você acertou', qtd_acertos)
+print('de', len(perguntas), 'perguntas.')
 
