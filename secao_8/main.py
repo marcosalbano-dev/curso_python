2dbf13ad733364ed135f19cf0bc7100240caab43
Marcos Albano <marcos.telematica@gmail.com>
2023-07-19 15:20:26 -0300

    add secao_8

---
+++ secao_8/main.py
@@ -0,0 +1,11 @@
+﻿import psycopg2
+
+conn = psycopg2.connect(dbname='sige', user='marcos.albano', password='Albano@1971', port='5432', host='172.28.1.160')
+cursor = conn.cursor()
+
+if cursor:
+    print('Conexão OK!')
+
+
+cursor.close()
+conn.close()
