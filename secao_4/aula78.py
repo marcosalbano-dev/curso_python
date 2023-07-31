26a08ce5446dcd74ea0306ce75ad2e2b43a280e0
Marcos Albano <marcos.telematica@gmail.com>
2023-07-31 10:50:46 -0300

    Add aulas 80 a 96 e modificado aula 78

--- secao_4/aula78.py
+++ secao_4/aula78.py
@@ -10,10 +10,10 @@
 # s1 = set('Luiz')
 # s1 = set()  # vazio
 # l1 =  [1, 2, 3, 3, 3, 3, 1]
-s1 = {1, 2, 3}
+s1 = set()
 # print(3 not in s1)
-for numero in s1:
-    print(numero)
+# for numero in s1:
+#     print(numero)
 
 # Sets são eficientes para remover valores duplicados
 # de iteráveis.
@@ -25,9 +25,21 @@ for numero in s1:
 
 # Métodos úteis:
 # add, update, clear, discard
+s1.add(1)
+s1.update(('teste', 1,2,3))
+# s1.clear()
+s1.discard('teste')
+# print(s1)
+
 
 # Operadores úteis:
 # união | união (union) - Une
 # intersecção & (intersection) - Itens presentes em ambos
 # diferença - Itens presentes apenas no set da esquerda
 # diferença simétrica ^ - Itens que não estão em ambos
+s1 = {1, 2, 3}
+s2 = {2, 3, 4}
+s3 = s1 | s2
+s3 = s1 & s2
+
+print(s3)
